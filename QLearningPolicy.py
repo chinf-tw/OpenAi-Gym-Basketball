9cd370ab8bbad75925bc38eb68cfe9250f65ffe7
CHINF <chinf1996@gmail.com>
2019-12-14 00:31:55 +0800

    create Q-Table from 1 to 3

--- QLearningPolicy.py
+++ QLearningPolicy.py
@@ -8,23 +8,61 @@ newState = None
 oldState = None
 speed = 0.1
 done = False
-qlearning = QLearning(env.col,env.row,9,learning_rate=0.01, reward_decay=0.9, e_greedy=0.5)
+isShoot = False
+
+qlearningBall = QLearning(env.col,env.row,9,learning_rate=0.1, reward_decay=0.9, e_greedy=0.2)
+qlearningShoot = QLearning(env.col,env.row,9,learning_rate=0.1, reward_decay=0.9, e_greedy=0.2)
+qlearningGetBall = QLearning(env.col,env.row,9,learning_rate=0.1, reward_decay=0.9, e_greedy=0.2)
+i = 0
+successAction = []
+reward = None
 while True:
     env.render()
     
     if newState is not None :
         if done :
             env.reset()
-            print(qlearning.qTable)
+            
+            i += 1
+            if i > 100 :
+                print("***100次了***")
+                i = 0
+            if reward == 10 or reward == 30 :
+                print(successAction)
+            successAction = []
+            # if not isShoot :
+            #     print(qlearningBall.qTable.astype(int))
+            # else:
+            #     print(qlearningShoot.qTable.astype(int))
+            #     pass
         oldState = newState
-        action = qlearning.epsGreedy(oldState)
+
+
+        if not isShoot :
+            action = qlearningBall.epsGreedy(oldState)
+        else:
+            action = qlearningShoot.epsGreedy(oldState)
+            pass
     else:
-        action = int(random.random()*8)
+        action = int(random.random()*1000) % 9
         pass
-    newState, reward, done, _ = env.step(action)
-    time.sleep(speed)
+    state, reward, done, _ = env.step(action)
+    newState,isShoot,isGetBall = state
+    successAction.append(action)
+    # if isShoot :
+    #     print(action)
+    #     i += 1
+    #     if i > 100 :
+    #         break
+
+    
     if oldState is not None :
-        qlearning.Learning(oldState,newState,action,reward)
+        if isGetBall :
+            qlearningGetBall.Learning(oldState,newState,action,reward)
+        elif isShoot :
+            qlearningShoot.Learning(oldState,newState,action,reward)
+        else:
+            qlearningBall.Learning(oldState,newState,action,reward)
+            pass
     pass
 
-# print(qlearning.stateToNumberConverter((1,1)))
